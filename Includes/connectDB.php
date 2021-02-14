40dc2e1ea64014d2afd002bbfe260d2fc83040fa
mustafaermira <67808882+mustafaermira@users.noreply.github.com>
2021-02-14 18:03:50 +0100

    Update connectDB.php

--- Includes/connectDB.php
+++ Includes/connectDB.php
@@ -3,12 +3,7 @@
 $dbServerName = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-<<<<<<< HEAD
-$dbName = "projekti4";
-=======
 $dbName = "projekti";
->>>>>>> 322616df2b675850f83f1b966e8642b4e97d122f
-
 
 $conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 
